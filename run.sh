c4ed6a15621f9ca89a8e3ed00f1539329625ac3b
Tu-Khiem Le <khiemletu@gmail.com>
2022-04-08 16:31:37 +0100

    :rocket: [Add] Update scripts for setting up vim and adding configurations

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+echo "==============================="
+echo "|      Getting VIM ready      |"
+echo "==============================="
+if ! command -v vim &> /dev/null
+then 
+	echo -n "[+] Installing vim..."
+	sudo apt-get install vim -y
+	echo "Done!"
+fi
+	
+	
+if [[ ! -d ~/.vim/bundle/Vundle.vim ]] 
+then 
+	echo -n "[+] Installing Vundle..."
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim  > /dev/null 2>&1
+	echo "Done!"
+fi
+
+echo -n "[+] Copying vim configs..."
+cp .vimrc ~/.vimrc
+echo "Done!"
+
+echo -n "[+] Installing plugins..."
+vim +PluginInstall +qall
+echo "Done!"
+
+
