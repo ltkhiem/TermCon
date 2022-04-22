5c375e3d4acceb2568f29ebf48978add99379049
ltkhiem <khiemletu@gmail.com>
2022-04-22 01:07:05 +0100

    :ambulance: [Fix] Handle gruvbox plugin error due to difference in vim's version
    Also added command to install latest vim

--- run.sh
+++ run.sh
@@ -12,12 +12,13 @@ then
 		brew install vim 
 		brew link vim
 	else
+		sudo add-apt-repository ppa:jonathonf/vim -y &> /dev/null
 		sudo apt-get install vim -y &> /dev/null
 	fi
 	echo "Done!"
 fi
 	
-	
+
 if [[ ! -d ~/.vim/bundle/Vundle.vim ]] 
 then 
 	echo -n "[+] Installing Vundle..."
@@ -25,13 +26,33 @@ then
 	echo "Done!"
 fi
 
+
+
 echo -n "[+] Copying vim configs..."
 if [[ -f ~/.vimrc ]]
 then 
 	mv ~/.vimrc ~/.vimrc.k.bak
 fi
+
 cp ~/TermCon/.vimrc ~
+
+vim_version=$(vim --version | head -1 | grep -o '[0-9]\.[0-9]')
+if [ ! $(echo "$vim_version < 8.2" | bc -l) ] 
+then 
+	p1s="\"colorscheme dracula"
+	p1r="colorscheme gruvbox"
+	p2s="autocmd vimenter \* ++nested colorscheme gruvbox"
+	p2r="\"autocmd vimenter \* ++nested colorscheme gruvbox"
+	if [[ "$OSTYPE" == "darwin"* ]]
+	then
+		sed -i ".k.tmp" "s/$p1s/$p1r/ ; s/$p2s/$p2r/" ~/.vimrc
+		rm ~/.vimrc.k.tmp
+	else
+		sed -i "s/$p1s/$p1r/ ; s/$p2s/$p2r/" ~/.vimrc
+	fi
+fi
 echo "Done!"
+
 
 echo -n "[+] Installing plugins..."
 vim +PluginInstall +qall
